1e4b02576b0233a62ffd7d35a7a8161515ef8d4b
Vandhuy <vandhuy@gmail.com>
2017-11-07 20:08:13 -0300

    Minor fixes

--- src/components/PokemonStoreList.vue
+++ src/components/PokemonStoreList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="products">
-    <div v-if="showLoading">Loading...</div>
+    <div v-if="showLoading">Loading Pokémons...</div>
     <div v-if="showNoResults">Sorry, no results were found.</div>
     <div v-if="!showLoading && !showNoResults" class="search-results">
       Found {{ results.length }} pokémons.
